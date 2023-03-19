962dc5717a067731cd28c6bd9c97cc54e7a9125a
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-03-19 18:32:19 +0100

    add newPhrase

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,30 @@
 /* SECCIÓN DE IMPORT */
 // import '...styles/App.scss';
-import { useState } from "react";
-import data from "../data/friends.json";
+import { useState, useEffect } from "react";
+// import data from "../data/friends.json";
 import "../styles/App.scss";
 
 /* SECCIÓN DEL COMPONENTE */
 function App() {
   /* VARIABLES ESTADO (DATOS) */
-  // const [quotes, setQuotes] = useState(data);
-
+  const [quotes, setQuotes] = useState([]);
   const [filterQuote, setFilterQuote] = useState("");
   const [filterCharacter, setFilterCharacter] = useState("todos");
+  const [newPhrase, setNewPhrase] = useState({
+    quote: "",
+    character: "",
+  });
+
   /* EFECTOS (día 5) */
+  useEffect(() => {
+    fetch(
+      "https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotes(data);
+      });
+  }, []);
 
   /* FUNCIONES HANDLER */
   const handleQuoteFilter = (ev) => {
@@ -20,10 +33,18 @@ function App() {
   const handleCharacterFilter = (ev) => {
     setFilterCharacter(ev.target.value);
   };
+  const handleNewPhrase = (ev) => {
+    setNewPhrase({ ...newPhrase, [ev.target.id]: ev.target.value });
+  };
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setQuotes([...quotes, newPhrase]);
+    setNewPhrase({ quotes: "", character: "" });
+  };
 
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
   const renderData = () => {
-    return data
+    return quotes
       .filter((eachData) => {
         return eachData.quote
           .toLocaleLowerCase()
@@ -80,6 +101,32 @@ function App() {
               <option value="Chandler">Chandler</option>
               <option value="Rachel">Rachel</option>
             </select>
+          </form>
+        </section>
+        <section className="newPhrase">
+          <h2>Añadir una nueva frase</h2>
+          <form action="">
+            <label htmlFor="phrases">Frase</label>
+            <input
+              type="text"
+              name="phrases"
+              id="phrases"
+              onChange={handleNewPhrase}
+              value={newPhrase.quote}
+            />
+            <label htmlFor="character">Personaje</label>
+            <input
+              type="text"
+              name="character"
+              id="character"
+              onChange={handleNewPhrase}
+              value={newPhrase.character}
+            />
+            <input
+              type="submit"
+              value="Añadir nueva frase"
+              onClick={handleClick}
+            />
           </form>
         </section>
       </main>
